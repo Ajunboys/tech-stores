297c6c819f915f88ba69193c71d17ef8c24168f3
ajunboys <weijunqiang2010@foxmail.com>
2021-02-25 19:40:27 +0800

    注解@RestControoler、@GetMapping使用

--- SpringBoot/demo/src/main/java/com/example/demo/DemoApplication.java
+++ SpringBoot/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,41 @@
 package com.example.demo;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Student;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	
+	/**
+	 * path:http://127.0.0.1:8080
+	 * @return
+	 */
+	@GetMapping
+	public String hello() {
+		return "hello world!";
+	}
+	
+	/**
+	 * path:http://127.0.0.1:8080/api/books/list
+	 * @return
+	 */
+	@GetMapping(path = "api/books/list")
+	public List<String> getBooks(){
+		return List.of("java", "c++", "javascript");
+	}
+
 
 }
